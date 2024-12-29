d8e59adb4c1be2567ce3d662bd607a3c40d39eb1
Katherine Lee <156264824+kthopeleee@users.noreply.github.com>
2024-12-29 10:27:28 -0800

    debugging navitation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,25 +6,29 @@ const Header = () => {
   return (
     <header className="header-container">
       {/* Logo Section */}
-      <a href="/TechnologyCenter" className="logo-link">
+      <a href="/TechnologyCenter/#/" className="logo-link">
         <img src={LOGO} alt="Columbia Policy Institute" />
       </a>
 
       {/* Navigation Links */}
       <nav className="nav-links">
-        <a href="/TechnologyCenter/initiatives" className="nav-item">Initiatives</a>
-        <a 
-          href="https://www.columbiapolicyinstitute.org/contact-us" 
-          target="_blank" 
-          rel="noopener noreferrer" 
+        {/* Internal Navigation Links */}
+        <a href="/TechnologyCenter/#/initiatives" className="nav-item">
+          Initiatives
+        </a>
+        {/* External Links */}
+        <a
+          href="https://www.columbiapolicyinstitute.org/contact-us"
+          target="_blank"
+          rel="noopener noreferrer"
           className="nav-item"
         >
           Contact Us
         </a>
-        <a 
-          href="https://www.columbiapolicyinstitute.org/" 
-          target="_blank" 
-          rel="noopener noreferrer" 
+        <a
+          href="https://www.columbiapolicyinstitute.org/"
+          target="_blank"
+          rel="noopener noreferrer"
           className="nav-item"
         >
           CPI Website
